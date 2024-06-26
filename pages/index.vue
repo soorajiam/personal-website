bafb96bb195e24a737765ec9dfecfef7deb73027
Sooraj P R <19621059+soorajiam@users.noreply.github.com>
2024-06-26 19:56:33 +0100

    managing migrations

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1 class="text-4xl">Hello World</h1>
+
+    <HomePageComponent />
+    
+  </div>
+</template>
+
+<script lang="ts" setup>
+import HomePageComponent from '~/components/homePageComponent.vue';
+
+
+</script>
+
+<style>
+
+</style>
