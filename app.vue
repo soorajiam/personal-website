9a38242f9f7dd90c1d359eb813f42f6ea3465de1
Sooraj P R <19621059+soorajiam@users.noreply.github.com>
2024-06-26 20:08:30 +0100

    resolving rendering issue

--- app.vue
+++ app.vue
@@ -1,15 +1,5 @@
 <template>
-  <head>
-    <title>Sooraj Parakkattil Ravi</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,400;9..40,500;9..40,600;9..40,700&display=swap" rel="stylesheet">
-    <!-- <link rel="stylesheet" href="css/tailwind/tailwind.min.css"> -->
-    <!-- <link rel="icon" type="image/png" sizes="32x32" href="shuffle-for-tailwind.png"> -->
 
-
-</head>
 <NuxtLayout>
     <NuxtPage />
     
